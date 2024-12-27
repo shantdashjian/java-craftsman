c509c0449cca6d99694d3d9f996b87be885e10b0
Shant Dashjian <shantgdashjian@gmail.com>
2024-12-27 13:21:30 -0800

    Favor format over concatenation

---
+++ src/main/java/chapter2/favorformatoverconcatenation/problem/Logbook.java
@@ -0,0 +1,7 @@
+package chapter2.favorformatoverconcatenation.problem;
+
+public class Logbook {
+    public void write(String entry) {
+        System.out.println(entry);
+    }
+}
---
+++ src/main/java/chapter2/favorformatoverconcatenation/solution/Logbook.java
@@ -0,0 +1,7 @@
+package chapter2.favorformatoverconcatenation.solution;
+
+public class Logbook {
+    public void write(String entry) {
+        System.out.println(entry);
+    }
+}
