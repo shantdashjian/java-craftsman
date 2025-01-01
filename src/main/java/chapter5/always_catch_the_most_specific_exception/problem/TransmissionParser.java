a5a8d11c59af7dddb008e585449a7d0385429bbb
Shant Dashjian <shantgdashjian@gmail.com>
2024-12-31 19:45:00 -0800

    Always catch the most specific exception

---
+++ src/main/java/chapter5/always_catch_the_most_specific_exception/problem/TransmissionParser.java
@@ -0,0 +1,20 @@
+package chapter5.always_catch_the_most_specific_exception.problem;
+
+public class TransmissionParser {
+    static Transmission parse(String rawMessage) {
+        if (rawMessage != null
+                && rawMessage.length() != Transmission.MESSAGE_LENGTH) {
+            throw new IllegalArgumentException("Bad message received!");
+        }
+
+        String rawId = rawMessage.substring(0, Transmission.ID_LENGTH);
+        String rawContent = rawMessage.substring(Transmission.ID_LENGTH);
+        try {
+            int id = Integer.parseInt(rawId);
+            String content = rawContent.trim();
+            return new Transmission(id, content);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Bad message received!");
+        }
+    }
+}
---
+++ src/main/java/chapter5/always_catch_the_most_specific_exception/solution/Transmission.java
@@ -0,0 +1,9 @@
+package chapter5.always_catch_the_most_specific_exception.solution;
+
+public class Transmission {
+    public static final int MESSAGE_LENGTH = 144;
+    public static final int ID_LENGTH = 1;
+
+    public Transmission(int id, String content) {
+    }
+}
