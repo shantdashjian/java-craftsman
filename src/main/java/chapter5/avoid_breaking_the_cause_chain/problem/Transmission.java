dd61cb7dd98ed5e1a362b11cbce8df23e86d878b
Shant Dashjian <shantgdashjian@gmail.com>
2025-01-01 18:08:40 -0800

    Avoid breaking the cause chain

---
+++ src/main/java/chapter5/avoid_breaking_the_cause_chain/problem/Transmission.java
@@ -0,0 +1,9 @@
+package chapter5.avoid_breaking_the_cause_chain.problem;
+
+public class Transmission {
+    public static final int MESSAGE_LENGTH = 144;
+    public static final int ID_LENGTH = 1;
+
+    public Transmission(int id, String content) {
+    }
+}
---
+++ src/main/java/chapter5/avoid_breaking_the_cause_chain/solution/TransmissionParser.java
@@ -0,0 +1,27 @@
+package chapter5.avoid_breaking_the_cause_chain.solution;
+
+public class TransmissionParser {
+    static Transmission parse(String rawMessage) {
+        if (rawMessage != null
+                && rawMessage.length() != Transmission.MESSAGE_LENGTH) {
+            throw new IllegalArgumentException(
+                    String.format("Expected %d, but got %d characters in '%s'",
+                            Transmission.MESSAGE_LENGTH, rawMessage.length(),
+                            rawMessage)
+            );
+        }
+
+        String rawId = rawMessage.substring(0, Transmission.ID_LENGTH);
+        String rawContent = rawMessage.substring(Transmission.ID_LENGTH);
+        try {
+            int id = Integer.parseInt(rawId);
+            String content = rawContent.trim();
+            return new Transmission(id, content);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(
+                    String.format("Expected number, but got '%s' in '%s'",
+                            rawId, rawMessage), e
+            );
+        }
+    }
+}
